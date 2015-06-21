01a9ab20cd695c52ba9c6e9da8b6aace8e547615
Sean Dolan <sdolan5@users.noreply.github.com>
2015-06-21 00:26:56 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,14 +4,14 @@ setwd("~/UCI HAR Dataset/")
 
 ##No.1- Merge the training and the test sets to create one data set
 #Read in the Train data- plus Features and Activity
-features <- read.table('./features.txt',header=FALSE)
-activityType <- read.table('./activity_labels.txt',header=FALSE)
-subjectTrain <- read.table('./train/subject_train.txt',header=FALSE)
-xTrain <- read.table('./train/x_train.txt',header=FALSE)
-yTrain <- read.table('./train/y_train.txt',header=FALSE)
+features <- read.table("./features.txt",header=FALSE)
+activityType <- read.table("./activity_labels.txt",header=FALSE)
+subjectTrain <- read.table("./train/subject_train.txt",header=FALSE)
+xTrain <- read.table("./train/x_train.txt",header=FALSE)
+yTrain <- read.table("./train/y_train.txt",header=FALSE)
 
 #Assign column names
-colnames(activityType) <- c('activityId','activityType')
+colnames(activityType) <- c("activityId","activityType")
 colnames(subjectTrain) <- "subjectId"
 colnames(xTrain) <- features[,2] 
 colnames(yTrain) <- "activityId"
@@ -20,9 +20,9 @@ colnames(yTrain) <- "activityId"
 trainingData <- cbind(yTrain,subjectTrain,xTrain)
 
 #Read in the Test data
-subjectTest <- read.table('./test/subject_test.txt',header=FALSE)
-xTest <- read.table('./test/x_test.txt',header=FALSE)
-yTest <- read.table('./test/y_test.txt',header=FALSE)
+subjectTest <- read.table("./test/subject_test.txt",header=FALSE)
+xTest <- read.table("./test/x_test.txt",header=FALSE)
+yTest <- read.table("./test/y_test.txt",header=FALSE)
 
 #Assign column names 
 colnames(subjectTest) <- "subjectId"
@@ -48,7 +48,7 @@ colNames <- colnames(mergedData)
 
 ##No.3- Use descriptive activity names to name the activities in the Dataset
 #Merge the dataset with the activityType table 
-mergedData <- merge(mergedData,activityType,by='activityId',all.x=TRUE)
+mergedData <- merge(mergedData,activityType,by="activityId",all.x=TRUE)
 
 ##No.4- Appropriately label the dataset with descriptive variable names. 
 names(mergedData)<-gsub("^t", "time", names(mergedData))
@@ -63,10 +63,10 @@ names(mergedData)<-gsub("BodyBody", "Body", names(mergedData))
 mergedData2 <- mergedData[,names(mergedData) != "activityType"]
 
 #Summarize the new table to include just the mean of each variable
-tidy_data <- aggregate(mergedData2[,names(mergedData2) != c('activityId','subjectId')], by= list(activityId= mergedData2$activityId, subjectId= mergedData2$subjectId),mean)
+tidy_data <- aggregate(mergedData2[,names(mergedData2) != c("activityId","subjectId")], by= list(activityId= mergedData2$activityId, subjectId= mergedData2$subjectId),mean)
 
 #Merge the new table with activityType to reintroduce descriptive activity names
-tidy_data <- merge(tidy_data,activityType,by='activityId',all.x=TRUE)
+tidy_data <- merge(tidy_data,activityType,by="activityId",all.x=TRUE)
 
 #Export the tidy dataset 
 write.table(tidy_data, "tidy_data.txt", row.names=FALSE)
